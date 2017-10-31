77fd9bcf0ad0c5704684a4c2c6782bae81df6efa
Joshua Leffell <joshua.leffell@sri.com>
2017-10-31 14:32:26 -0700

    Added multiple GPU Keras examples from pyimagesearch

--- cats_vs_dogs/cnn/augmented_data/test_aug.py
+++ cats_vs_dogs/cnn/augmented_data/test_aug.py
@@ -58,11 +58,12 @@ test_data_dir = 'data/test'
 nb_train_samples = 20000
 nb_validation_samples = 2500
 nb_test_samples = 2500
-epochs = 100
-batch_size = 64
+epochs = 50
+batch_size = 32
+data_augmentation = True
 
 kernel_initializer = 'glorot_uniform'
-kernel_regularizer = regularizers.l2(0.0)
+kernel_regularizer = None # regularizers.l2(0.0)
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
@@ -70,39 +71,51 @@ else:
     input_shape = (img_width, img_height, 3)
 
 model = Sequential()
-model.add(Conv2D(64, (5, 5), input_shape=input_shape, kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer))
+model.add(Conv2D(8, (9, 9), input_shape=input_shape, kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128, (3, 3), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
+model.add(Conv2D(16, (7, 7), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128, (3, 3), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
+model.add(Conv2D(32, (5, 5), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(250, (3, 3), kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
+model.add(Activation('relu'))
 
 model.add(Flatten())
-model.add(Dense(128, kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(125, activation='relu', kernel_initializer=kernel_initializer,kernel_regularizer=kernel_regularizer))
+#model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
 
-sgd = SGD(lr=0.01)
+
+sgd = SGD(lr=0.01, momentum=0.9)
 # rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-8, decay=0.0)
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
               metrics=['accuracy'])
 
 model.summary()
 
 # this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.0,
-    zoom_range=0.0,
-    horizontal_flip=False)
+if data_augmentation :
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.0,
+        zoom_range=0.0,
+        rotation_range=10.0,
+        horizontal_flip=False)
+else:
+    train_datagen = ImageDataGenerator(
+                rescale=1. / 255)
+
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
@@ -112,13 +125,13 @@ train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 model.fit_generator(
     train_generator,
@@ -131,7 +144,7 @@ test_generator = test_datagen.flow_from_directory(
     test_data_dir,
     target_size=(img_width, img_height),
     batch_size=nb_test_samples,
-    class_mode='binary')
+    class_mode='categorical')
 
 # Extract test data
 testData, testLabels = test_generator.next()
